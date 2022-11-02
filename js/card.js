53ea1a100a65edbc59fc0136b76277e8f2efa12d
Rajath A R <92567686+rajathar4@users.noreply.github.com>
2022-11-02 12:46:35 +0530

    newUpdate

---
+++ js/card.js
@@ -0,0 +1,11 @@
+function myFunction1() {
+    location.replace("https://rajathar4.github.io/2021-Last-Message/")
+  }
+
+  function myFunction2() {
+    location.replace("https://rajathar4.github.io/task1/")
+  }
+
+  function myFunction3() {
+    location.replace("https://github.com/rajathar4")
+  }
